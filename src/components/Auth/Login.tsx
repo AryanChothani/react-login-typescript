c3cefa16c84fa31101b87419ea5cfff5f275e530
Arya4514 <63945059+Arya4514@users.noreply.github.com>
2022-08-31 18:26:16 +0530

    added warning for maximum wrong credentials

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -5,34 +5,52 @@ import { useNavigate } from "react-router-dom";
 import { addUser } from '../../redux/slices/UserSlice'
 import AuthService from "../../services/Auth";
 import './Auth.css'
-
+import Session from 'store2';
 
 
 export default function () {
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
-    const [creds, setCreds] = useState({});
+    const [creds, setCreds] = useState({ email: "", password: "" });
+    const [attempts, setAttempts] = useState(10);
+    const [invaliedCreds, setInvaliedCreds] = useState(false);
+    const [restrictLogin, setRestrictLogin] = useState(false);
+
+
 
     const handleChange = (e: any, key: string) => {
         setCreds({ ...creds, [key]: e.target.value });
     }
 
     const handleSubmit = async () => {
+        if (attempts == 0) {
+            setRestrictLogin(true)
+        } else {
+            try {
+                let data = await AuthService.Login(creds);
 
-        let data = await AuthService.Login(creds);
-
-        if (!data.error) {
-            dispatch(addUser(creds))
-            navigate('/otp')
+                if (!data.error) {
+                    dispatch(addUser({ email: creds.email }))
+                    Session.session("session_token", data.sessionToken)
+                    navigate('/otp')
+                } else {
+                    let wrongAttempts = attempts
+                    setAttempts(wrongAttempts - 1)
+                }
+            } catch (error) {
+                let wrongAttempts = attempts
+                setAttempts(wrongAttempts - 1)
+                setInvaliedCreds(true)
+                console.log(error)
+            }
         }
-
     }
 
 
     return (
-        <div className="Auth-form-container">
-            <form className="Auth-form">
+        <div className="Auth-form-container" >
+            <form className="Auth-form needs-validation">
                 <div className="Auth-form-content">
                     <h3 className="Auth-form-title">Sign In</h3>
                     <div className="form-group mt-3">
@@ -58,11 +76,15 @@ export default function () {
                             Submit
                         </button>
                     </div>
-                    <p className="forgot-password text-right mt-2">
-                        Forgot <a href="#">password?</a>
-                    </p>
+                    &nbsp;
+                    {invaliedCreds ? <div className="alert alert-danger" role="alert">
+                        Please provide a valid email and password. You have {attempts} attempts left.
+                    </div> : ""}
+                    {restrictLogin ? <div className="alert alert-danger" role="alert">
+                        Reached the maximum limit of attempts. Please try again after some time.
+                    </div> : ""}
                 </div>
-            </form>
-        </div>
+            </form >
+        </div >
     )
 }
